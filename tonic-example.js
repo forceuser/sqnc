711a18547ca2550419203028e90c7d65af82a124
forceuser <forceuserz@gmail.com>
2017-06-30 15:43:21 +0300

    1.0.6 release commit

--- tonic-example.js
+++ tonic-example.js
@@ -1,26 +1,4 @@
-const ad = require("active-data");
+const sqnc = require("sqnc");
 
-ad.setOptions({
-	immediateAutorun: true // make recalculations for each change
-});
-
-const data = ad.makeObservable({
-	welcomeMessage: "Hello,",
-	firstName: "Luke",
-	lastName: "Skywalker"
-});
-
-ad.makeComputed(data, "fullName", self => `${self.firstName} ${self.lastName}`);
-
-ad.makeAutorun(() => {
-	console.log(data.welcomeMessage + " " + data.fullName);
-});
-// "Hello, Luke Skywalker" will be printed immediately (can be configured)
-
-data.firstName = "Leia"; // will print "Hello, Leia Skywalker"
-
-ad.run(() => {
-	// group changes and run autorun function only at the end
-	data.firstName = "Anakin";
-	data.welcomeMessage = "Welcome to dark side,";
-});
+console.log(sqnc("👶", "👰"));
+console.log(sqnc(n => Math.pow(2, n), 10));
